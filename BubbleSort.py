2201518db4fe6dd2e478725511d616d50b5b9d2f
snehalds <sneds@eg.dk>
2025-02-19 15:14:40 +0530

    Update BubbleSort.py

--- BubbleSort.py
+++ BubbleSort.py
@@ -3,10 +3,10 @@ def bubbleSort(ar):
    # Traverse through all array elements
    for i in range(n):
    # Last i elements are already in correct position
-   for j in range(0, n-i-1):
-      # Swap if the element found is greater than the next element
-      if ar[j] > ar[j+1] :
-         ar[j], ar[j+1] = ar[j+1], ar[j]
+      for j in range(0, n-i-1):
+         # Swap if the element found is greater than the next element
+         if ar[j] > ar[j+1] :
+            ar[j], ar[j+1] = ar[j+1], ar[j]
 # Driver code to test above
 ar = ['t','u','t','o','r','i','a','l']
 bubbleSort(ar)
